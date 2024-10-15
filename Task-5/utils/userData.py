3e296867b04e0d002f6944e4d25de5542bf01805
sejal-rokade <sejalrokade1@gmail.com>
2024-10-15 13:09:10 +0530

    complete

---
+++ Task-5/utils/userData.py
@@ -0,0 +1,12 @@
+import re
+def getNumericData(user_input):
+
+    try:
+        data=''.join(filter(str.isdigit,user_input))
+
+        return data
+    except:
+        return print("no numeric value")
+        
+       
+    
